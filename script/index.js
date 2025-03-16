72551fca4c8741c90da8d9e5e4d82c0a27d36af1
ShahadatHasan623 <shahadathasan743599@gmail.com>
2025-03-16 08:47:03 +0100

    added lesson figure

--- script/index.js
+++ script/index.js
@@ -12,7 +12,6 @@ function displayCategories(data) {
             const categoryDiv = document.createElement("div")
             categoryDiv.innerHTML = `
              <button  onclick="loadData(${cat.level_no})" class="btn btn-outline btn-primary"><img src="./assets/fa-book-open.png" alt="">lesson-${cat.level_no}</button>
-             
             `
             cetagoryContainer.appendChild(categoryDiv)
       }
@@ -22,21 +21,43 @@ function displayCategories(data) {
 
 function loadData (id){
       fetch(`https://openapi.programming-hero.com/api/level/${id}`)
-      .then(response =>response.json())
+      .then(response =>response.json())                                                                                      
       .then(data => cardDisplay(data.data))
-
 }
 const cardDisplay =(cards)=>{
-      const button = document.getElementById("category-container")
+      const cardAdd = document.getElementById("card-container")
+      cardAdd.innerHTML="";
+      document.getElementById("lessnStart").style.display ="none"
       cards.forEach(card => {
-            const div = document.createElement("div");
-            div.innerHTML=`${card.word}`
-            button.append(div)
-
+            console.log(card)
+            const div =document.createElement("div")
+            div.innerHTML =`<div class="card w-96 h-50 bg-base-200 card-xl shadow-xl">
+      <div class="card-body ">
+      <h1 class="card-title flex justify-center"> ${card.word}</h1>
+      <p>meaning/pronunciation</p>
+      <h2>${card.meaning} / ${card.pronunciation}</h2>
+      <div class="flex justify-between items-center">
+       <div>
+           <i class="fa-solid fa-circle"></i>
+      </div>
+      <div>
+      <p><i class="fa-solid fa-volume-high"></i></p>
+      </div>
+      </div>
+    
+      </div>
+      </div>`
+            cardAdd.appendChild(div)
       });
+      if(cards.length === 0){
+            cardAdd.innerHTML =` <div class="flex flex-col justify-center items-center col-span-full py-5">
+            <img src="./assets/alert-error.png" alt="">
+            <p>এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</p>
+            <h1 class="text-3xl font-bold">নেক্সট Lesson এ যান</h1>
+           </div>`
+           return;
+      }
 
 }
 
-loadData()
 showCatogry()
-
--- script/login.js
+++ script/login.js
@@ -3,9 +3,11 @@ document.getElementById("getStarted")
       const password = document.getElementById("password").value ;
       const convertPassword =parseInt(password);
       const hiddenClass = document.querySelectorAll(".content")
+      const bannerSection = document.getElementById("banner")
       if(convertPassword == 1234){
             hiddenClass.forEach(hiddenClass=>{
                   hiddenClass.classList.remove("hidden")
+                  bannerSection.style.display ="none"
             })
       }
       else{
