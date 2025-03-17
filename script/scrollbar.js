c350575990acda637ef8173daf421f4720fea12b
ShahadatHasan623 <shahadathasan743599@gmail.com>
2025-03-17 07:42:01 +0100

    added update navbar

---
+++ script/scrollbar.js
@@ -0,0 +1,9 @@
+document.getElementById("faq-btn").addEventListener("click", function () {
+      document.getElementById("faq-section").scrollIntoView({ behavior: "smooth" });
+});
+
+document.getElementById("learn-btn").addEventListener("click", function () {
+      document.getElementById("learn-section").scrollIntoView({ behavior: "smooth" });
+});
+
+
